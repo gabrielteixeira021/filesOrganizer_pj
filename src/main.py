7a0c300d2d3ef887979690139120991aa346e1a2
gabrielTeixeira0021 <charlesgabriel102@gmail.com>
2025-08-31 17:24:19 -0300

    Add DOWNLOADS_DIR definition to main function for file organization

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
 """
 File Organizer - Automatically organize downloaded files by type
 """
+import os    
 
 def main():
     """Main function for file organizer."""
     print("File Organizer - Ready to organize your downloads!")
     # TODO: file organization logic here
     
+    DOWNLOADS_DIR = os.path.expanduser("~/Downloads")    
     
 
 if __name__ == "__main__":
